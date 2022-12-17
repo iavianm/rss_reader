4340f40a0a80bb7a1fc260d81fa7aa25c67e477e
iavianm <iavianm@mail.ru>
2022-12-17 13:55:22 +0200

    add news

---
+++ db/migrate/20221217114000_create_news.rb
@@ -0,0 +1,17 @@
+class CreateNews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :news do |t|
+      t.string :title
+      t.string :link
+      t.string :author
+      t.string :description
+      t.date   :pubdate
+
+      t.references :channel, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :news, :title
+    add_index :news, :pubDate,                  unique: true
+  end
+end
