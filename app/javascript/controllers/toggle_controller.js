cadbc66f8ccfce48b530edaa910e99317464922e
iavianm <iavianm@mail.ru>
2022-12-19 23:03:52 +0200

    add stimulus

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "navbarNavAltMarkup" ]
+
+  connect() {
+    this.navbarNavAltMarkupTarget.addEventListener('click', () => {
+      document.getElementById('navbarNavAltMarkup').classList.toggle('show')
+    })
+  }
+}
