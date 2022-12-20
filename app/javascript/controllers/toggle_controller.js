d14bce4c97dc0a808574062bd20bbf2ed42362ac
iavianm <iavianm@mail.ru>
2022-12-20 14:11:14 +0200

    update views and add validations

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -4,6 +4,9 @@ export default class extends Controller {
   static targets = [ "navbarNavAltMarkup" ]
 
   connect() {
+
+    console.log("navbarNavAltMarkup")
+
     this.navbarNavAltMarkupTarget.addEventListener('click', () => {
       document.getElementById('navbarNavAltMarkup').classList.toggle('show')
     })
