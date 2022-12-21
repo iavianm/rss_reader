13687f58f1df008d357910df3d008c92af69abcf
iavianm <iavianm@mail.ru>
2022-12-21 16:51:15 +0200

    update tests

--- spec/views/channels/edit.html.slim_spec.rb
+++ spec/views/channels/edit.html.slim_spec.rb
@@ -10,25 +10,26 @@ RSpec.describe('channel/edit', type: :view) do
     find('.btn-style-edit').click
     expect(page).to(have_content('Editing channel'))
   end
+  describe 'click links on page' do
+    it 'click link Editing channel with incorrect URL' do
+      visit channels_path
+      find('.btn-style-edit').click
+      expect(page).to(have_content('Editing channel'))
+      find('.form-title').set('Drive')
+      find('.form-url').set('http://www.drive.ru')
+      find('.btn-success').click
+      expect(page).to(have_content('Editing channel'))
+    end
 
-  it 'click link Editing channel with incorrect URL' do
-    visit channels_path
-    find('.btn-style-edit').click
-    expect(page).to(have_content('Editing channel'))
-    find('.form-title').set('Drive')
-    find('.form-url').set('http://www.drive.ru')
-    find('.btn-success').click
-    expect(page).to(have_content('Editing channel'))
-  end
-
-  it 'click link Editing channel with correct URL' do
-    visit channels_path
-    find('.btn-style-edit').click
-    expect(page).to(have_content('Editing channel'))
-    find('.form-title').set('Drive')
-    find('.form-url').set('http://feeds.feedburner.com/drivenet')
-    find('.btn-success').click
-    expect(page).to(have_content('Drive'))
-    expect(page).to(have_content('RSS channels'))
+    it 'click link Editing channel with correct URL' do
+      visit channels_path
+      find('.btn-style-edit').click
+      expect(page).to(have_content('Editing channel'))
+      find('.form-title').set('Drive')
+      find('.form-url').set('http://feeds.feedburner.com/drivenet')
+      find('.btn-success').click
+      expect(page).to(have_content('Drive'))
+      expect(page).to(have_content('RSS channels'))
+    end
   end
 end
